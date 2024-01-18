a58509cba6b2c838db1103f84808c9c174447a9b
BISHWA BANDHU PARMAR <bishwabandhuparmar06@gmail.com>
2024-01-18 22:55:23 +0530

    Add files via upload
    run npm init in your code editor terminal

---
+++ demo/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = 3000;
+//var path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}) 
